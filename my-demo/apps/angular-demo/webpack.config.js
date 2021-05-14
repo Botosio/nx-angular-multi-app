ca83f1a6e572cb2fa8e1cce5d2e7e4453dc088de
Developer.Dave 🧙‍♂️ <44526601+Botosio@users.noreply.github.com>
2021-05-14 08:14:10 -0400

    ng add @angular-architects/module-federation --project angular-demo --port 3000

---
+++ my-demo/apps/angular-demo/webpack.config.js
@@ -0,0 +1,52 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.base.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "angularDemo",
+    publicPath: "auto"
+  },
+  optimization: {
+    runtimeChunk: false
+  },   
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      
+        // For remotes (please adjust)
+        // name: "angularDemo",
+        // filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './apps/angular-demo/src/app/app.component.ts',
+        // },        
+        
+        // For hosts (please adjust)
+        // remotes: {
+        //     "myDemo": "myDemo@http://localhost:5000/remoteEntry.js",
+        //     "nxDemo": "nxDemo@http://localhost:4201/remoteEntry.js",
+
+        // },
+
+        shared: {
+          "@angular/core": { singleton: true, strictVersion: true }, 
+          "@angular/common": { singleton: true, strictVersion: true }, 
+          "@angular/common/http": { singleton: true, strictVersion: true }, 
+          "@angular/router": { singleton: true, strictVersion: true },
+
+          ...sharedMappings.getDescriptors()
+        }
+        
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
